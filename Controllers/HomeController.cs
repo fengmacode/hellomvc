7519ebf8f6eba5f06e1f258fbc2052c7a7fafb68
jason <18926229912@189.cn>
2016-04-29 22:10:50 +0800

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvc
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index() => View();
+        
+        [HttpGet("/about")]
+        public ActionResult About(){
+            return Json("ok");
+        }
+    }
+}
